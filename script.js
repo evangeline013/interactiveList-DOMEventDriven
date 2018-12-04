e32edf2d5019f48b6b3733bf1311452049214689
Eva <evangeline013@gmail.com>
2018-12-04 12:00:21 -0500

    List items are striked through on click. Undo with a second click.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
 
 function inputLength() {
 	return input.value.length;
@@ -10,6 +11,7 @@ function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
+	addStrikeThrough(li);
 	input.value = "";
 }
 
@@ -23,6 +25,17 @@ function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
+}
+
+function addStrikeThrough(listItem) {
+	listItem.addEventListener("click", function() {
+		this.classList.toggle("done");
+	});
+}
+
+//Add event listener (click then strike through) to all the list items in HTML files.
+for (var i = 0; i < li.length; i++) {
+	addStrikeThrough(li[i]);
 }
 
 button.addEventListener("click", addListAfterClick);
