8d39fdef14273fc713d03ef8ab97283e827fd332
Eva <evangeline013@gmail.com>
2018-12-04 12:28:43 -0500

    Add delete button with event listener to list item

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var li = document.getElementsByTagName("li");
+var deleteButton = document.getElementsByClassName("delete");
 
 function inputLength() {
 	return input.value.length;
 }
 
+function createDltBtn() {
+	let btn = document.createElement("button");
+	btn.classList.add("delete");
+	btn.appendChild(document.createTextNode("Delete"));
+	removeListAfterClickDltBtn(btn);
+	return btn;
+}
+
 function createListElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
+	li.appendChild(document.createTextNode(`${input.value} `));
+	li.appendChild(createDltBtn());
 	ul.appendChild(li);
 	addStrikeThrough(li);
 	input.value = "";
@@ -33,9 +43,21 @@ function addStrikeThrough(listItem) {
 	});
 }
 
+function removeListAfterClickDltBtn(btn) {
+	btn.addEventListener("click", function() {
+		var parentList = btn.parentNode;
+		parentList.parentNode.removeChild(parentList);
+	});
+}
+
 //Add event listener (click then strike through) to all the list items in HTML files.
 for (var i = 0; i < li.length; i++) {
 	addStrikeThrough(li[i]);
+}
+
+//Add event listener to all the delete buttons in HTML file.
+for (var i = 0; i < deleteButton.length; i++) {
+	removeListAfterClickDltBtn(deleteButton[i]);
 }
 
 button.addEventListener("click", addListAfterClick);
